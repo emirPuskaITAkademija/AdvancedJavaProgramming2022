9bace70424349e09fee8d77436309dc742f6a779
silegrb <sisicfarisofficial@gmail.com>
2022-12-22 19:48:06 +0100

    Interakcija s BAZOM podataka: Tabela customers, baza store, JDBC Java java.sql

---
+++ src/main/java/org/example/three/homework/Container.java
@@ -0,0 +1,11 @@
+package org.example.three.homework;
+
+import java.util.Iterator;
+
+public class Container<E> implements Iterable<E>{
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
---
+++ src/main/java/org/example/three/jdbc/DBDemo.java
@@ -0,0 +1,12 @@
+package org.example.three.jdbc;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class DBDemo {
+    public static void main(String[] args) throws SQLException {
+        CustomerMySQLReader customerMySQLReader = new CustomerMySQLReader();
+        List<Customer> customers = customerMySQLReader.loadCustomers();
+        customers.forEach(customer -> System.out.println(customer));
+    }
+}
