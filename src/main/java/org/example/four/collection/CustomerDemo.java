51945f37db583a253ca88102379154236426e0e7
silegrb <sisicfarisofficial@gmail.com>
2022-12-26 20:45:24 +0100

    DAO pattern: customers via Customer

---
+++ src/main/java/org/example/four/collection/CustomerDemo.java
@@ -0,0 +1,25 @@
+package org.example.four.collection;
+
+import org.example.three.jdbc.Customer;
+
+import java.util.List;
+
+public class CustomerDemo {
+    public static void main(String[] args) {
+        CustomerDao customerDao = new CustomerDao();
+        Customer customer = customerDao.get(5);
+        List<Customer> customers = customerDao.getAll();
+        //iterator
+//        for (Customer c : customers) {
+//            printCustomer(c);
+//        }
+
+        //spliterator
+        //Consumer<Customer> customerConsumer = c -> printCustomer(c);
+        customers.forEach(CustomerDemo::printCustomer);
+    }
+
+    static void printCustomer(Customer customer) {
+        System.out.println(customer);
+    }
+}
---
+++ src/main/java/org/example/four/collection/Dao.java
@@ -0,0 +1,31 @@
+package org.example.four.collection;
+
+import org.example.three.jdbc.Customer;
+
+import java.util.List;
+
+/**
+ * CRUD
+ * <p>
+ *     Create
+ *     Retrieve
+ *     Update
+ *     Delete
+ * </p>
+ * @param <E>
+ */
+public interface Dao<E> {
+    String URL = "jdbc:mysql://localhost:3306/store";
+    String USERNAME = "root";
+    String PASSWORD = "root";
+
+    E create(E entity);
+
+    List<E> getAll();
+
+    E get(long primaryKey);
+
+    E update(E entity);
+
+    void delete(E entity);
+}
